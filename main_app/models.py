17c1d05ea8095b7ac99f6d146fcec863e383b0ab
Tyler Reid <tylerreid1988@gmail.com>
2020-10-10 14:36:57 -0400

    add record model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Record: 
+    def __init__(self, title, release_year, genre, review):
+        self.title = title
+        self.release_year = release_year
+        self.genre = genre
+        self.review = review
+
+records = [
+    Record('Aja', 1977, 'jazz-rock?', 'A reaally good album'),
+    Record('blue', 1977, 'folk-jazz?', 'pretty good album. Good color'),
+    Record('Sail Away', 1970, 'rock', 'one of the best ')
+]
