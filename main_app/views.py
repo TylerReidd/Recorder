1395cd41014f5f4de8379a67e1b3401e3fea227e
Tyler Reid <tylerreid1988@gmail.com>
2020-10-10 14:26:11 -0400

    add record index

--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def records_index(request):
+  return render(request, 'records/index.html', { 'records': records })
