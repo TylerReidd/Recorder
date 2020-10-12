33db87d65e4cd4ad6865b86b25691d4fdb8dd59a
Tyler Reid <tylerreid1988@gmail.com>
2020-10-12 16:26:51 -0400

    add debug

--- recordcollector/settings.py
+++ recordcollector/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 import environ
+
 env = environ.Env(
     DEBUG=(bool, False)
 )
@@ -28,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY= env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
